8146591c0bf58d71841b1c090fb142292834d0b7
William Vearrier <william.vearrier@gmail.com>
2020-05-07 01:33:26 -0500

    Updates

---
+++ precache-manifest.3c54f5e86c384db3d3e5ee69b81e1912.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55bf7bd9d1257806b7adb02e0a6d3be4",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "5dbb176c2523589d464e",
+    "url": "/pokedex/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "312b0a490f0c1bd1ec77",
+    "url": "/pokedex/static/js/2.6d62aeb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.6d62aeb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dbb176c2523589d464e",
+    "url": "/pokedex/static/js/main.2dd0f515.chunk.js"
+  },
+  {
+    "revision": "014345e0bd0e463ebfd2",
+    "url": "/pokedex/static/js/runtime-main.83eb840c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokedex/static/media/logo.5d5d9eef.svg"
+  }
+]);
